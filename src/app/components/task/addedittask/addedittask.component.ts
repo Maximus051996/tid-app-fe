05ccc2cc5bd4d6dc8fd278fcc6f3542aec14c569
sayan pramanick <sayanp05@gmail.com>
2024-08-25 02:09:31 +0530

    Login Page UI changes and basic component creation and route changes -- 25.08.2024

---
+++ src/app/components/task/addedittask/addedittask.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addedittask',
+  standalone: true,
+  imports: [],
+  templateUrl: './addedittask.component.html',
+  styleUrl: './addedittask.component.scss'
+})
+export class AddedittaskComponent {
+
+}
